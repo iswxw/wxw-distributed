36d2b7a2c8fc9e79d60ac2412e245db95ed80987
魏小伟 <1550518923@qq.com>
2020-11-16 16:43:01 +0800

    分布式锁实现

---
+++ cloud-concurrent-redis/src/main/java/com/wxw/common/distributed_lock/RedissionTest01.java
@@ -0,0 +1,30 @@
+package com.wxw.common.distributed_lock;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * @author ：wxw.
+ * @date ： 16:34 2020/11/16
+ * @description：分布式锁测试
+ * @version: v_0.0.1
+ */
+@Component
+public class RedissionTest01 {
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    public void test_lock(){
+        RLock lock = redissonClient.getLock("-1");
+        lock.unlock();
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
