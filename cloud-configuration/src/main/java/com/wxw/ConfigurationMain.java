b266d2c3387f497989501dfbb6c40931c4f4940f
魏小伟 <1550518923@qq.com>
2020-12-09 17:57:42 +0800

    延迟队列

---
+++ cloud-configuration/src/main/java/com/wxw/ConfigurationMain.java
@@ -0,0 +1,14 @@
+package com.wxw;
+
+/**
+ * @author ：wxw.
+ * @date ： 15:00 2020/12/9
+ * @description：统一配置
+ * @link:
+ * @version: v_0.0.1
+ */
+public class ConfigurationMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ cloud-delayqueue/src/main/java/com/wxw/common/job_delay_queue/JobDelayQueueDemo.java
@@ -0,0 +1,29 @@
+package com.wxw.common.job_delay_queue;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ：wxw.
+ * @date ： 17:10 2020/12/9
+ * @description：基于定时任务的延迟队列
+ * @link:
+ * @version: v_0.0.1
+ */
+@Component
+public class JobDelayQueueDemo {
+
+    /**
+     * 每隔五秒
+     */
+    @Scheduled(cron = "0/5 * * * * ? ")
+    public void process(){
+
+        /**
+         * 下单时间，如果当前时间超过当前时间
+         */
+        String submitOrder = "2020-07-22 13:00:00";
+
+        System.out.println("我是定时任务！\n 我来看一下延迟时间够了没有！！" );
+    }
+}
