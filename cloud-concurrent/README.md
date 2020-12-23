b2fe15166644b4891d6108f7c5e156d875fcd87b
魏小伟 <815372184@qq.com>
2020-12-23 16:56:47 +0800

    基于redis+token 实现幂等性

---
+++ cloud-concurrent/README.md
@@ -0,0 +1,28 @@
+### 并发编程实战
+
+
+
+#### 1. Redis分布式锁
+
+#### 2. Zookeeper 分布式锁
+
+#### 3. Redis 布隆过滤器
+
+#### 4. Redis 限流
+
+#### 5. Redis 幂等性
+
+
+
+**相关文章**
+
+1. [Token保证幂等实现](https://www.oschina.net/question/3089641_2320565?sort=default) 
+2. [接口幂等性解决方案](https://github.com/GitHubWxw/wxw-document/tree/master/%E9%A1%B9%E7%9B%AE%E5%AE%9E%E8%B7%B5/%E6%9C%8D%E5%8A%A1%E6%B2%BB%E7%90%86/%E5%B9%82%E7%AD%89%E6%80%A7) 
+
+
+
+
+
+
+
+更多内容福利：[快速访问](https://wwxw.gitee.io/wxw-document) 
---
+++ cloud-concurrent/src/test/java/com/wxw/token/TestIdempotency.java
@@ -0,0 +1,48 @@
+package com.wxw.token;
+
+import com.wxw.common.Idempotency.TokenUtilService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @author ：wxw.
+ * @date ： 16:32 2020/12/23
+ * @description：幂等性测试
+ * @link:
+ * @version: v_0.0.1
+ */
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestIdempotency {
+
+    @Resource
+    private TokenUtilService tokenUtilService;
+
+    // 用户值
+    private static String userInfo = "wxw";
+
+    /**
+     * 生成token
+     */
+    @Test
+    public void testIdempotency(){
+        String generateToken = tokenUtilService.generateToken(userInfo);
+        log.info("generateToken = {}", generateToken);
+    }
+
+    /**
+     * 验证token
+     */
+    @Test
+    public void testUserInfo(){
+        boolean validToken = tokenUtilService.validToken("290aa479-e410-4188-b6bc-8b2f306f8c58", userInfo);
+        log.info("validToken = {}",validToken);
+    }
+
+}
