a33ffd4c47d938bbf4062743c3f251b9fc30cb46
魏小伟 <815372184@qq.com>
2021-02-03 11:34:22 +0800

    redis 分布式锁的自动续约、主存异步复制导致锁丢失，多客户端加锁导致产生脏数据

--- cloud-concurrent/src/main/java/com/wxw/common/redission_distributed_lock/RedissionRedLockTest.java
+++ cloud-concurrent/src/main/java/com/wxw/common/redission_distributed_lock/RedissionRedLockTest.java
@@ -1,9 +1,12 @@
 package com.wxw.common.redission_distributed_lock;
 
 import org.redisson.Redisson;
+import org.redisson.RedissonMultiLock;
 import org.redisson.RedissonRedLock;
 import org.redisson.api.RLock;
 import org.redisson.config.Config;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author ：wxw.
@@ -18,7 +21,7 @@ public class RedissionRedLockTest {
      * Redis 通过红锁解决了 主从节点下异步 同步数据导致锁丢失问题
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String lockKey = "myLock";
         Config config = new Config();
         config.useSingleServer().setPassword("123456").setAddress("redis://127.0.0.1:6379");
@@ -27,11 +30,40 @@ public class RedissionRedLockTest {
         Config config3 = new Config();
         config.useSingleServer().setPassword("123456").setAddress("redis://127.0.0.1:6381");
 
-        RLock lock = Redisson.create(config).getLock(lockKey);
+        /**
+         * 大家都知道，如果负责储存这个分布式锁的Redis节点宕机以后，而且这个锁正好处于锁住的状态时，这个锁会出现锁死的状态。为了避免这种情况的发生，
+         * Redisson内部提供了一个监控锁的看门狗，它的作用是在Redisson实例被关闭前，不断的延长锁的有效期。默认情况下，看门狗的检查锁的超时时间是30秒钟，
+         * 也可以通过修改Config.lockWatchdogTimeout来另行指定
+         */
+        config.setLockWatchdogTimeout(30000L); // 设置为30s时检查一次，key是否过期
+
+
+        RLock lock1 = Redisson.create(config).getLock(lockKey);
         RLock lock2 = Redisson.create(config2).getLock(lockKey);
         RLock lock3 = Redisson.create(config3).getLock(lockKey);
 
-        RedissonRedLock redLock = new RedissonRedLock(lock, lock2, lock3);
+        /**
+         * =================== 8.4. 红锁（RedLock）==============
+         * 基于Redis的Redisson红锁RedissonRedLock对象实现了Redlock介绍的加锁算法。该对象也可以用来将多个RLock对象关联为一个红锁，
+         * 每个RLock对象实例可以来自于不同的Redisson实例。
+         * Redis 通过 红锁 解决了 主从节点下异步 同步数据导致锁丢失问题
+         */
+        RedissonRedLock redLock = new RedissonRedLock(lock1, lock2, lock3);
+
+        /**
+         * ================8.3. 联锁（MultiLock）================
+         */
+        RedissonMultiLock multiLock = new RedissonMultiLock(lock1, lock2, lock3);
+        // 同时加锁：lock1 lock2 lock3
+        // 所有的锁都上锁成功才算成功
+
+        // 给lock1，lock2，lock3加锁，如果没有手动解开的话，10秒钟后将会自动解开
+        multiLock.lock(10, TimeUnit.SECONDS);
+
+        // 为加锁等待100秒时间，并在加锁成功10秒钟后自动解开
+        boolean res = multiLock.tryLock(100, 10, TimeUnit.SECONDS);
+
+
 
         try {
             /**
--- cloud-concurrent/src/main/java/com/wxw/common/redission_distributed_lock/RedissionTest01.java
+++ cloud-concurrent/src/main/java/com/wxw/common/redission_distributed_lock/RedissionTest01.java
@@ -1,16 +1,20 @@
 package com.wxw.common.redission_distributed_lock;
 
+import org.redisson.api.RCountDownLatch;
 import org.redisson.api.RLock;
+import org.redisson.api.RReadWriteLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author ：wxw.
  * @date ： 16:34 2020/11/16
  * @description：分布式锁测试
- * @version: v_0.0.1
+ * @link: https://github.com/redisson/redisson/wiki/
  */
 @Component
 public class RedissionTest01 {
@@ -18,12 +22,79 @@ public class RedissionTest01 {
     @Resource
     private RedissonClient redissonClient;
 
-    public void test_lock(){
+    public void test_lock() throws InterruptedException {
+
 
         /**
+         * ====================可重入锁（Reentrant Lock）=========================
          * 获取锁
          */
-        RLock lock = redissonClient.getLock("-1");
+        RLock lock = redissonClient.getLock("anyLock");
+
+        /**
+         * 加锁以后10秒钟自动解锁
+         * 无需调用unlock方法手动解锁
+         */
+        lock.lock(10, TimeUnit.SECONDS);
+
+        // 尝试加锁，最多等待100秒，上锁以后10秒自动解锁
+        boolean res = lock.tryLock(100, 10, TimeUnit.SECONDS);
+        if (res) {
+            try {
+                // TODO: 2021/2/3 执行业务逻辑
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        /**
+         * 上面情况也支持异步加锁和释放锁
+         */
+        lock.lockAsync();
+        lock.lockAsync(10, TimeUnit.SECONDS);
+        Future<Boolean> resAsync = lock.tryLockAsync(100, 10, TimeUnit.SECONDS);
+
+        /**
+         * ===================公平锁（Fair Lock）===================
+         */
+        RLock fairLock = redissonClient.getFairLock("anyLock");
+        // 最常见的使用方法
+        fairLock.lock();
+
+        /**
+         * =================== 读写锁（ReadWriteLock）==============
+         * 基于Redis的Redisson分布式可重入读写锁RReadWriteLock Java对象实现了java.util.concurrent.locks.ReadWriteLock接口。
+         * 其中读锁和写锁都继承了RLock接口。
+         * 分布式可重入读写锁允许同时有多个读锁和一个写锁处于加锁状态。
+         */
+        RReadWriteLock rwlock = redissonClient.getReadWriteLock("anyRWLock");
+        // 最常见的使用方法
+        rwlock.readLock().lock();
+        // 或
+        rwlock.writeLock().lock();
+
+        // 10秒钟以后自动解锁
+        // 无需调用unlock方法手动解锁
+        rwlock.readLock().lock(10, TimeUnit.SECONDS);
+        // 或
+        rwlock.writeLock().lock(10, TimeUnit.SECONDS);
+        // 尝试加锁，最多等待100秒，上锁以后10秒自动解锁
+        boolean res1 = rwlock.readLock().tryLock(100, 10, TimeUnit.SECONDS);
+        // 或
+        boolean res2 = rwlock.writeLock().tryLock(100, 10, TimeUnit.SECONDS);
+
+        /**
+         * ============== 8.8. 闭锁（CountDownLatch）===============
+         * 基于Redisson的Redisson分布式闭锁（CountDownLatch）Java对象RCountDownLatch采用了
+         * 与 java.util.concurrent.CountDownLatch相似的接口和用法。
+         */
+        RCountDownLatch latch1 = redissonClient.getCountDownLatch("anyCountDownLatch");
+        latch1.trySetCount(1);
+        latch1.await();
+        // 在其他线程或其他JVM里
+        RCountDownLatch latch2 = redissonClient.getCountDownLatch("anyCountDownLatch");
+        latch2.countDown();
+
 
         /**
          * 释放锁
