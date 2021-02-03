e041c274c5d14174e461c218c1fd2023585e911f
魏小伟 <815372184@qq.com>
2021-02-03 16:39:49 +0800

    watchdog

---
+++ cloud-concurrent/src/main/java/com/wxw/common/redission_distributed_lock/RedissionSource.java
@@ -0,0 +1,16 @@
+package com.wxw.common.redission_distributed_lock;
+
+import org.redisson.client.handler.ConnectionWatchdog;
+
+/**
+ * @author ：wxw.
+ * @date ： 14:12 2021/2/3
+ * @description：Redission 源码验证
+ * @link:
+ * @version: v_0.0.1
+ */
+public class RedissionSource {
+    public static void main(String[] args) {
+
+    }
+}
