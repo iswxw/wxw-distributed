cf8769d13361c40b234fb44c02b3647bc6dbb191
weixiaowei <weixiaowei@didichuxing.com>
2022-01-08 18:28:48 +0800

    add sign

---
+++ cloud-safety/src/main/java/com/wxw/common/utils/ApiEncryptUtil.java
@@ -0,0 +1,20 @@
+package com.wxw.common.utils;
+
+import java.util.Random;
+
+/**
+ * @author ：weixiaowei.
+ * @date ：2022/1/8
+ * @description：OpenApi通信协议加解密工具类
+ * @version: 1.0.0
+ */
+public class ApiEncryptUtil {
+
+    private final static Random random = new Random();
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
