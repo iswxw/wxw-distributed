7a74fdda963ff5c4884678909c4cd81b556d6463
weixiaowei <weixiaowei@didichuxing.com>
2022-01-09 23:35:59 +0800

    add test

---
+++ cloud-safety/src/main/java/com/wxw/test/TestMain.java
@@ -0,0 +1,10 @@
+package com.wxw.test;
+
+/**
+ * @author ：weixiaowei.
+ * @date ：2022/1/9
+ * @description：
+ * @version: 1.0.0
+ */
+public class TestMain {
+}
---
+++ cloud-safety/src/main/java/com/wxw/test/dd220109/TestDecryptAndVerify.java
@@ -0,0 +1,14 @@
+package com.wxw.test.dd220109;
+
+/**
+ * @author ：weixiaowei.
+ * @date ：2022/1/9
+ * @description：
+ * @version: 1.0.0
+ */
+public class TestDecryptAndVerify {
+
+    public static void main(String[] args) {
+
+    }
+}
